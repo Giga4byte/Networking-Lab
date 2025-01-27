28da73da0778469e29aaf0b96a6d86a8d21cce4a
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-01-27 11:07:19 +0530

    Update UDPclient.c

--- Day 4/UDPclient.c
+++ Day 4/UDPclient.c
@@ -1,49 +1,71 @@
-// CLIENT SIDE
+// client
 
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <unistd.h>
 #include <arpa/inet.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <unistd.h>
 
 #define PORT 8080
 #define SERVER_ADDRESS "127.0.0.1"
+#define BUFFER_SIZE 1024
 
 int main() {
-    int sock;
-    struct sockaddr_in serv_addr;
-    char buffer[1024] = {0};
-    char message[1024];  // Initialize a buffer to store the client input
-
-    // Prompt the user to enter the message
-    printf("Enter the message to send: ");
-    scanf("%[^\n]", message);  // Read the entire line, including spaces
-
-    // Creating socket file descriptor
-    if ((sock = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        printf("\nSocket creation error\n");
-        return -1;
-    }
-
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_port = htons(PORT);
-
-    // Convert IPv4 and IPv6 addresses from text to binary form
-    if (inet_pton(AF_INET, SERVER_ADDRESS, &serv_addr.sin_addr) <= 0) {
-        printf("\nInvalid address/ Address not supported\n");
-        return -1;
-    }
-
-    // Send the message to the server
-    sendto(sock, (const char *)message, strlen(message), 0, (const struct sockaddr *)&serv_addr, sizeof(serv_addr));
-    printf("Message sent to server: %s\n", message);
-
-    // Receive the response from the server
-    int n;
-    socklen_t len = sizeof(serv_addr);
-    n = recvfrom(sock, (char *)buffer, 1024, 0, (struct sockaddr *)&serv_addr, &len);
-    buffer[n] = '\0';  // Null-terminate the received data
-    printf("Server response: %s\n", buffer);
-
-    return 0;
+    int client_socket;
+    struct sockaddr_in server_addr;
+    char buffer[BUFFER_SIZE];
+    socklen_t server_addr_len = sizeof(server_addr); 
+    
+    client_socket = socket(AF_INET, SOCK_DGRAM, 0);
+	if (client_socket < 0) {
+		printf("\terror...\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_port = htons(PORT);
+	
+	if (inet_pton(AF_INET, SERVER_ADDRESS, &server_addr.sin_addr) <= 0) {
+		printf("\tinvalid address...\n");
+		close(client_socket);
+		exit(EXIT_FAILURE);
+	}
+	
+	while (1) {
+		printf("Client: ");
+		fgets(buffer, BUFFER_SIZE, stdin);
+		buffer[strcspn(buffer, "\n")] = '\0';
+		
+		ssize_t bytes_sent = sendto(client_socket, buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&server_addr, server_addr_len);
+		if (bytes_sent < 0) {
+			printf("\terror...\n");
+			break;
+		}
+		
+		if (strcmp(buffer, "exit") == 0) {
+			printf("Messages have ended\n");
+			break;
+		}
+		
+		memset(buffer, 0, BUFFER_SIZE);
+		
+		ssize_t bytes_received = recvfrom(client_socket, buffer, BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&server_addr, &server_addr_len);
+		if (bytes_received < 0) {	
+			printf("\terror...\n");
+			break;
+		}
+		buffer[bytes_received] = '\0';
+		printf("Server: %s\n", buffer);
+		
+		if (strcmp(buffer, "exit") == 0) {
+			printf("Messages have ended\n");
+			break;
+		}
+	}
+	
+	close(client_socket);
+	
+	return 0;
 }
