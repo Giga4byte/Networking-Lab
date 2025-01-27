6e41365a7e930ac953c1676c3d8ec21a302be9a4
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-01-27 11:07:35 +0530

    Update UDPserver.c

--- Day 4/UDPserver.c
+++ Day 4/UDPserver.c
@@ -3,52 +3,69 @@
 #include <string.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <netinet/in.h>
+#include <sys/socket.h>
 
 #define PORT 8080
 #define MAX_BUFFER_SIZE 1024
 
 int main() {
-    int server_fd;
-    struct sockaddr_in address, client_addr;
-    int addrlen = sizeof(address);
-    char buffer[MAX_BUFFER_SIZE] = {0};
-    char response[MAX_BUFFER_SIZE];
-
-    if ((server_fd = socket(AF_INET, SOCK_DGRAM, 0)) < 0) {
-        perror("socket failed");
-        exit(EXIT_FAILURE);
-    }
-
-    address.sin_family = AF_INET;
-    address.sin_addr.s_addr = INADDR_ANY;
-    address.sin_port = htons(PORT);
-
-    if (bind(server_fd, (struct sockaddr *)&address, sizeof(address)) < 0) {
-        perror("bind failed");
-        exit(EXIT_FAILURE);
-    }
-
-    printf("Server is listening on port %d...\n", PORT);
-
-    while (1) {
-        printf("Enter the string (or 'exit' to quit): ");
-        scanf(" %s", response); 
-
-        if (strcmp(response, "exit") == 0) {
-            printf("EXITING FROM THE CHAT!\n");
-            break;
-        }
-
-        int n;
-        socklen_t client_len = sizeof(client_addr);
-        n = recvfrom(server_fd, (char *)buffer, MAX_BUFFER_SIZE, 0, (struct sockaddr *)&client_addr, &client_len);
-        buffer[n] = '\0';
-        printf("Client: %s\n", buffer);
-
-        sendto(server_fd, (const char *)response, strlen(response), 0, (const struct sockaddr *)&client_addr, client_len);
-        printf("Response sent to client: %s\n", response);
-    }
-
-    close(server_fd);
-    return 0;
+	int server_fd, bind_status;
+	struct sockaddr_in serv_addr, client_addr;
+	socklen_t client_addr_len = sizeof(client_addr);
+	char buffer[MAX_BUFFER_SIZE];
+	
+	server_fd = socket(AF_INET, SOCK_DGRAM, 0);
+	if (server_fd < 0) {
+		printf("\terror...\n");
+		exit(EXIT_FAILURE);
+	}
+	
+	serv_addr.sin_family = AF_INET;
+	serv_addr.sin_addr.s_addr = INADDR_ANY;
+	serv_addr.sin_port = htons(PORT);
+	
+	bind_status = bind (server_fd, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+	if (bind_status < 0) {
+		printf("\terror...\n");
+		close(server_fd);
+		exit(EXIT_FAILURE);
+	}
+	
+	while(1) {
+		memset(buffer, 0, MAX_BUFFER_SIZE);
+		
+		ssize_t bytes_received = recvfrom(server_fd, buffer, MAX_BUFFER_SIZE, MSG_WAITALL, (struct sockaddr *)&client_addr, &client_addr_len);
+		if(bytes_received < 0) {
+			printf ("\treceive failed...\n");
+			break;
+		}
+		
+		buffer[bytes_received] = '\0';
+		printf ("Client: %s\n", buffer);
+		
+		if (strcmp(buffer, "exit") == 0) {
+			printf("Messages have ended\n");
+			break;
+		}
+		
+		printf("Server: ");
+		fgets(buffer, MAX_BUFFER_SIZE, stdin);
+		buffer[strcspn(buffer, "\n")] = '\0';
+		
+		ssize_t bytes_sent = sendto(server_fd, buffer, strlen(buffer), MSG_CONFIRM, (const struct sockaddr *)&client_addr, client_addr_len);
+		if(bytes_sent < 0) {
+			printf("\tsend error...\n");
+			break;
+		}
+		
+		if(strcmp(buffer, "exit") == 0) {
+			printf("Messages have ended\n");
+			break;
+		}
+	}
+	
+	close(server_fd);
+	
+	return 0;
 }
