f482f7efb42e63572d8d5c9f16159c7f5c9384dd
Giribala Arun <120817536+Giga4byte@users.noreply.github.com>
2025-04-06 17:47:07 +0530

    Update dvr.c

--- Day 6/dvr.c
+++ Day 6/dvr.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-#define INFINITY 9999
+#define INF 9999
 #define MAX_NODES 10
 
 typedef struct {
@@ -29,5 +29,47 @@ int main() {
       rt[i].nextHop[j] = (costMatrix[i][j] == INF) ? -1 : j;
     } 
   }
+
+  // Output: Cost matrix
+  for (i=0; i<nodes; i++) {
+    for(j=0; j<nodes; j++) {
+      printf("%d\t", costMatrix[i][j]);
+    }
+    printf("\n");
+  }
+
+  // Routing algorithm
+  int updated;
+  do {
+    updated = 0;
+      for (i = 0; i < nodes; i++) {
+        for (j = 0; j < nodes; j++) {
+          for (k = 0; k < nodes; k++) {
+            if (rt[i].distance[k] != INF && costMatrix[k][j] != INF) {
+              if (rt[i].distance[j] > rt[i].distance[k] + costMatrix[k][j]) {
+                rt[i].distance[j] = rt[i].distance[k] + costMatrix[k][j];
+                rt[i].nextHop[j] = k;
+                updated = 1;
+              } 
+            } 
+          } // k loop
+        } // j loop
+      } // i loop
+    } while (updated); 
+  
+    // Output: Final Routing Tables
+    printf("\nRouting Tables after Convergence:\n");
+    for (i = 0; i < nodes; i++) {
+      printf("\nRouter %d:\n", i);
+      printf("Destination\tNext Hop\tDistance\n");
+      for (j = 0; j < nodes; j++)
+      {
+        if (rt[i].distance[j] == INF) {
+          printf("%d\t\t-\t\tINF\n", j);
+        } else {
+          printf("%d\t\t%d\t\t%d\n", j, rt[i].distance[j], rt[i].nextHop[j]);
+        } 
+      } //j loop
+    } //i loop
   return 0;
 }
